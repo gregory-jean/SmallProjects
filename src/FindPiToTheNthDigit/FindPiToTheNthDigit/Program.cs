34a906443963fa7d5ff37f00e2c043768c2c37f8
Gregory Jean <gregforlife87@gmail.com>
2019-02-16 21:05:52 -0800

    Initial commit of Find PI to n length.

---
+++ src/FindPiToTheNthDigit/FindPiToTheNthDigit/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace FindPiToTheNthDigit
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int piDigitLength = 0;
+            do
+            {
+                Console.WriteLine("What digit would you like to find Pi to?");
+                string PiDigitLengthString = Console.ReadLine();
+
+                try
+                {
+                    piDigitLength = int.Parse(PiDigitLengthString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Input, enter a number using the numerical pad.");
+                }
+            }
+            while (piDigitLength == 0);
+
+            Console.WriteLine(GetPie(piDigitLength));
+        }
+
+        private static string GetPie(int piDigitLength)
+        {
+            string output;
+            try
+            {
+                // TODO: find way to calc Pi
+                output = $"{Math.PI}".Substring(0, piDigitLength + 1);
+            }
+            catch (Exception)
+            {
+                output = null;
+            }
+
+            return output;
+        }
+    }
+}
